254ea05f31042d52b3bc6f2817ad8719ea2bf299
kazuhiro fukumoto <>
2022-07-26 16:00:57 +0900

    MyErrorViewコンポーネントを作成

---
+++ components/MyErrorView.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- ロードエラー時 -->
+    <v-col align="center">
+        <div class="display-1">{{ errorTitle }}</div>
+        <p>{{ errorMessage }}</p>
+        <v-btn rounded color="primary" dark @click="reload">
+            再読込
+        </v-btn>
+    </v-col>
+</template>
+
+<script>
+export default {
+    name: 'MyErrorView',
+    props: {
+        errorTitle: {
+            type: String,
+            required: false,
+            default: "エラー"
+        },
+        errorMessage: {
+            type: String,
+            required: false,
+            default: "予期せぬエラーが発生しました"
+        }
+    },
+    methods: {
+        reload() {
+            this.$emit('reload');
+        }
+    }
+}
+</script>
