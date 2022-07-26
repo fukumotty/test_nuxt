cf5cd14cd3bc5857d785a8680526e2a2426cba9a
kazuhiro fukumoto <>
2022-07-26 16:01:19 +0900

    MyLoadingProgressコンポーネントを作成

---
+++ components/MyLoadingProgress.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-col align="center">
+        <!-- 読み込み時 -->
+        <v-progress-circular :size="size" :width="width" indeterminate :color="color"></v-progress-circular>
+    </v-col>
+</template>
+
+<script>
+export default {
+    name: 'MyLoadingProgress',
+    props: {
+        size: {
+            type: Number,
+            required: false,
+            default: 50
+        },
+        width: {
+            type: Number,
+            required: false,
+            default: 5
+        },
+        color: {
+            type: String,
+            required: false,
+            default: "grey"
+        }
+    },
+}
+</script>
