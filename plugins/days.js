3b4340b566787b17fe5b701a9b836c9bad5ae2ba
kazuhiro fukumoto <>
2022-07-31 17:23:44 +0900

    不具合修正

--- plugins/days.js
+++ plugins/days.js
@@ -1,6 +1,9 @@
 export default ({ app }, inject) => {
-    inject("dateformat_YYYYMMDD_HHmmsss", (dateString) => {
-        const jpnDate = new Date(new Date(dateString).toLocaleString({ timeZone: 'Asia/Tokyo' })).getTime();
+    inject("dateformat_YYYYMMDD_HHmmsss", dateString => {
+        if (dateString == null) {
+            return '';
+        }
+        const jpnDate = new Date(new Date(dateString).toLocaleString({ timeZone: 'Asia/Tokyo' }));
         return jpnDate.getFullYear().toString().padStart(2, '0') + "/"
             + (jpnDate.getMonth() + 1).toString().padStart(2, '0') + "/"
             + jpnDate.getDay().toString().padStart(2, '0') + " "
