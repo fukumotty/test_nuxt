fe10fe8ad1996ff73e05be9849cc958616429c52
kazuhiro fukumoto <>
2022-07-31 17:50:44 +0900

    pluginsにconstants.jsを追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/constants.js',
     '~/plugins/axios.js',
     '~/plugins/days.js',
   ],
---
+++ plugins/constants.js
@@ -0,0 +1,49 @@
+export default ({ app }, inject) => {
+    inject("const", {
+        rssInfo: [
+            {
+                name: "はてな（人気）",
+                category: "hatena-hot",
+                targetUrl: "/apiHatena/hotentry/it.rss"
+            },
+            {
+                name: "はてな（新着）",
+                category: "hatena-new",
+                targetUrl: "/apiHatena/entrylist/it.rss"
+            },
+            {
+                name: "YahooNews（主要）",
+                category: "yahoo-news-top",
+                targetUrl: "/apiYahooNews/rss/topics/top-picks.xml"
+            },
+            {
+                name: "YahooNews（国内）",
+                category: "yahoo-news-domestic",
+                targetUrl: "/apiYahooNews/rss/categories/domestic.xml"
+            },
+            {
+                name: "YahooNews（国際）",
+                category: "yahoo-news-world",
+                targetUrl: "/apiYahooNews/rss/categories/world.xml"
+            },
+            {
+                name: "ITMedia（新着）",
+                category: "it-media-new",
+                targetUrl: "/apiITMedia/rss/2.0/news_bursts.xml"
+            },
+        ]
+    });
+
+    // indexから情報を取得
+    inject("getRssInfoFromIndex", (index) => {
+        return app.$const.rssInfo.length > index ? app.$const.rssInfo[index] : undefined;
+    });
+    // categoryから情報を取得
+    inject("getRssInfoFromCategory", (category) => {
+        return app.$const.rssInfo.find(element => element.category === category);
+    });
+    // categoryからindex情報を取得
+    inject("getRssInfoIndexFromCategory", (category) => {
+        return app.$const.rssInfo.findIndex(element => element.category === category);
+    });
+};
