0eda64e1ff94e122f33926872f3f689bcc2872a5
kazuhiro fukumoto <>
2022-08-01 15:07:11 +0900

    rss取得処理をstore内で行うようにする

--- pages/rss/_category.vue
+++ pages/rss/_category.vue
@@ -39,8 +39,6 @@ import MyCardView from '~/components/MyCardView'
 import MyErrorView from '~/components/MyErrorView'
 import MyLoadingProgress from '~/components/MyLoadingProgress'
 
-const parseString = require('xml2js').parseString;
-
 export default {
     name: 'RssListPage',
     components: {
@@ -61,10 +59,12 @@ export default {
                 title: "エラー",
                 message: null,
             },
-            items: [],
         }
     },
     computed: {
+        items() {
+            return this.$store.getters['rss/getRssResultData'](this.$route.params.category);
+        },
         listDisplayFlg: {
             get() {
                 return this.$store.getters['rss/getListDisplayFlg']();
@@ -84,89 +84,13 @@ export default {
         // データを取得する
         load() {
             const category = this.$route.params.category;
-            const rssInfo = this.$getRssInfoFromCategory(category);
-
             this.loadingFlg = true;
             this.initError();
-
-            this.$axios.get(rssInfo.targetUrl).then((response) => {
-                parseString(response.data, (err, result) => {
-                    this.parse(err, category, result);
-                });
-            }).catch((err) => {
+            this.$store.dispatch('rss/getRssData', category).catch((err) => {
                 this.setError(err.errorMessage);
             }).finally(() => {
                 this.loadingFlg = false;
             });
-        },
-        // パース処理
-        parse(err, category, resultJson) {
-            if (category.includes('hatena')) {
-                this.parseHatena(err, resultJson);
-            } else if (category.includes('yahoo-news')) {
-                this.parseYahooNews(err, resultJson);
-            } else if (category.includes('it-media')) {
-                this.parseITMedia(err, resultJson);
-            } else {
-                this.setError('予期せぬエラーが発生しました。');
-            }
-        },
-        // はてなブックマークを整形
-        parseHatena(err, resultJson) {
-            this.items.splice(0);
-            if (err) {
-                this.setError('予期せぬエラーが発生しました。');
-            } else {
-                const jsonItem = resultJson["rdf:RDF"].item;
-                for (const key in jsonItem) {
-                    const item = jsonItem[key];
-                    this.items.push({
-                        title: item.title === undefined ? "" : item.title[0],
-                        description: item.description === undefined ? "" : item.description[0],
-                        link: item.link === undefined ? "" : item.link[0],
-                        image: item.image === undefined ? "" : item.image[0],
-                        date: item["dc:date"] === undefined ? "" : this.$dateformat_YYYYMMDD_HHmmsss(item["dc:date"][0]),
-                    });
-                }
-            }
-        },
-        // Yahooニュースを整形
-        parseYahooNews(err, resultJson) {
-            this.items.splice(0);
-            if (err) {
-                this.setError('予期せぬエラーが発生しました。');
-            } else {
-                const jsonItem = resultJson.rss.channel[0].item;
-                for (const key in jsonItem) {
-                    const item = jsonItem[key];
-                    this.items.push({
-                        title: item.title === undefined ? "" : item.title[0],
-                        description: item.description === undefined ? "" : item.description[0],
-                        link: item.link === undefined ? "" : item.link[0],
-                        image: item.image === undefined ? "" : item.image[0],
-                        date: item.pubDate === undefined ? "" : this.$dateformat_YYYYMMDD_HHmmsss(item.pubDate[0]),
-                    });
-                }
-            }
-        },
-        // ITMedia
-        parseITMedia(err, resultJson) {
-            this.items.splice(0);
-            if (err) {
-                this.setError('予期せぬエラーが発生しました。');
-            } else {
-                const jsonItem = resultJson.rss.channel[0].item;
-                for (const key in jsonItem) {
-                    const item = jsonItem[key];
-                    this.items.push({
-                        title: item.title === undefined ? "" : item.title[0],
-                        description: item.description === undefined ? "" : item.description[0],
-                        link: item.link === undefined ? "" : item.link[0],
-                        image: item.image === undefined ? "" : item.image[0],
-                        date: item.pubDate === undefined ? "" : this.$dateformat_YYYYMMDD_HHmmsss(item.pubDate[0]),
-                    });
-                }
-            }
         },
         initError() {
             this.error.flg = false;
