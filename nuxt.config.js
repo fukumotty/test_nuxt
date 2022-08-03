e7595ac9bcd659d30267a05012978aea5d80a846
kazuhiro fukumoto <>
2022-08-04 00:07:17 +0900

    customPropertiesの設定追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,6 +93,9 @@ export default {
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
         }
+      },
+      options: {
+        customProperties: true
       }
     }
   },
