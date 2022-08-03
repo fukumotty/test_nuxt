87caf8103fa41ea36709da94455a50343eb70d69
kazuhiro fukumoto <>
2022-08-04 00:03:53 +0900

    axiosのplugin内で、loadingFlgの更新処理を行う

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,20 +1,27 @@
-export default function ({ $axios, redirect }) {
+export default function ({ $axios, store, redirect }) {
 
     $axios.defaults.timeout = 100000;
 
     $axios.onRequest((config) => {
-        // console.log('Making request to ' + JSON.stringify(config));
+        store.commit("view/setLoadingFlg", true);
     });
 
     $axios.onResponse(response => {
+        store.commit("view/setLoadingFlg", false);
         return Promise.resolve(response);
     });
 
     $axios.onError((error) => {
+        store.commit("view/setLoadingFlg", false);
         const code = parseInt(error.response && error.response.status)
         if (code === 400) {
             redirect('/400')
         }
+        return Promise.reject(error.response);
+    });
+
+    $axios.onResponseError((error) => {
+        store.commit("view/setLoadingFlg", false);
         return Promise.reject(error.response);
     });
 }
