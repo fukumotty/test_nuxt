d69b20018a75bc5155e5f9886dd1a1604334fe21
kazuhiro fukumoto <>
2022-08-04 17:59:41 +0900

    アルバム一覧ページを追加

---
+++ pages/jsonplaceholder/albums/index.vue
@@ -0,0 +1,113 @@
+<template>
+    <v-row>
+        <!-- 読み込み時 -->
+        <my-loading-progress v-if='loadingFlg' :color="$const.loadingProgress.color"
+            :width="$const.loadingProgress.width" :size="$const.loadingProgress.size">
+        </my-loading-progress>
+        <!--  Error発生時 -->
+        <my-error-view v-else-if='error.flg' :error-title="error.title" :error-message="error.message" @reload="load">
+        </my-error-view>
+        <v-container v-else-if="items.length > 0">
+            <v-row class="my-3">
+                <v-btn outlined color="primary" @click="detailLink()">API
+                </v-btn>
+            </v-row>
+            <v-row class="my-3">
+                <v-card>
+                    <v-card-title>
+                        Albums LIST
+                        <v-spacer></v-spacer>
+                        <v-text-field v-model="search" append-icon="mdi-magnify" label="検索" single-line hide-details>
+                        </v-text-field>
+                    </v-card-title>
+                    <v-data-table :headers="headers" :items="items" :search="search" no-data-text="表示するデータがありません。"
+                        no-results-text="一致するデータがありません。">
+                        <template v-slot:[`item.userId`]="{ item }">
+                            <nuxt-link :to="'/jsonplaceholder/user/' + item.userId">
+                                {{ item.userId }}
+                            </nuxt-link>
+                        </template>
+                        <template v-slot:[`item.title`]="{ item }">
+                            <nuxt-link :to="'/jsonplaceholder/albums/' + item.id">
+                                {{ item.title }}
+                            </nuxt-link>
+                        </template>
+                    </v-data-table>
+                </v-card>
+            </v-row>
+        </v-container>
+        <v-container v-else>
+            <v-row>
+                <div class="text-h3 mt-3">表示するデータがありません。</div>
+            </v-row>
+        </v-container>
+    </v-row>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import MyErrorView from '~/components/MyErrorView'
+import MyLoadingProgress from '~/components/MyLoadingProgress'
+
+export default {
+    name: 'JsonPlaceholderAlbumsListPage',
+    components: {
+        MyErrorView,
+        MyLoadingProgress,
+    },
+    data() {
+        return {
+            search: '',
+            headers: [
+                {
+                    text: 'id', value: 'id',
+                },
+                {
+                    text: 'userId', value: 'userId',
+                },
+                {
+                    text: 'title', value: 'title',
+                }
+            ],
+            error: {
+                flg: false,
+                title: "エラー",
+                message: null,
+            },
+        }
+    },
+    computed: {
+        ...mapGetters({
+            items: "jsonplaceholder/getAlbumItems",
+            loadingFlg: "view/getLoadingFlg",
+        }),
+    },
+    mounted() {
+        this.load();
+    },
+    methods: {
+        ...mapActions({
+            getAlbums: "jsonplaceholder/getAlbums",
+        }),
+        // データを取得する
+        load() {
+            this.initError();
+            this.getAlbums().catch((err) => {
+                this.setError(err === undefined ? "" : err.errorMessage);
+            });
+        },
+        initError() {
+            this.error.flg = false;
+            this.error.message = null;
+        },
+        setError(message) {
+            this.error.flg = true;
+            this.error.message = message;
+        },
+        // 外部サイトを開く
+        detailLink() {
+            window.open('https://jsonplaceholder.typicode.com/albums', '_blank');
+        }
+    }
+}
+</script>
